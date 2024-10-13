582eb7800a1f4954930e45c534f3f41381361226
Zakaria Asad <zasad2587@gmail.com>
2024-10-13 20:30:57 +0300

    Refactor: extract the hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,7 @@
-import CategoryHeading from "@/components/CategoryHeading";
-import FeatureImages from "@/components/featureImages";
-import SectionsHeading from "@/components/SectionsHeading";
-import {
-  faucetsImage,
-  floorsImage,
-  kitchensImage,
-  pavementsImage,
-  sharubsImage,
-  sinksImage,
-  wallsImage,
-} from "@/utils/imagesArray";
-import Image from "next/image";
-import heroImage from "../../public/Rectangle 31.png";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <Image src={heroImage} alt="Hero Image" width={1920} height={958} />
-      <CategoryHeading>INTERIOR CLADDING</CategoryHeading>
-      <SectionsHeading>FLOORS</SectionsHeading>
-      <FeatureImages imagesName={floorsImage} />
-      <SectionsHeading>WALLS</SectionsHeading>
-      <FeatureImages imagesName={wallsImage} />
-      <SectionsHeading>SINKS</SectionsHeading>
-      <FeatureImages imagesName={sinksImage}/>
-      <SectionsHeading>FAUCETS</SectionsHeading>
-      <FeatureImages imagesName={faucetsImage} />
-      <SectionsHeading>KITCHENS</SectionsHeading>
-      <FeatureImages imagesName={kitchensImage} />
-      <CategoryHeading>LANDSCAPING DEPARTMENT</CategoryHeading>
-      <SectionsHeading>SHARUBS</SectionsHeading>
-      <FeatureImages imagesName={sharubsImage} />
-      <SectionsHeading>SHARUBS</SectionsHeading>
-      <FeatureImages imagesName={pavementsImage} />
-    </div>
+    <Hero />
   );
 }
