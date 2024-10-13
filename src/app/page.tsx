81e14067ef14d7bf03e083c42e972182adea8bf5
Zakaria Asad <zasad2587@gmail.com>
2024-10-13 20:26:14 +0300

    Refactor: extract the feuture image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,104 +1,41 @@
 import CategoryHeading from "@/components/CategoryHeading";
+import FeatureImages from "@/components/featureImages";
 import SectionsHeading from "@/components/SectionsHeading";
-import { faucetsImage, floorsImage, kitchensImage, pavementsImage, sharubsImage, sinksImage, wallsImage } from "@/utils/imagesArray";
+import {
+  faucetsImage,
+  floorsImage,
+  kitchensImage,
+  pavementsImage,
+  sharubsImage,
+  sinksImage,
+  wallsImage,
+} from "@/utils/imagesArray";
 import Image from "next/image";
 import heroImage from "../../public/Rectangle 31.png";
 
 export default function Home() {
-  
-
   return (
     <div className="flex flex-col">
       <Image src={heroImage} alt="Hero Image" width={1920} height={958} />
       <CategoryHeading>INTERIOR CLADDING</CategoryHeading>
       <SectionsHeading>FLOORS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {floorsImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={floorsImage} />
       <div className="flex mt-5 items-center">
         <div className="bg-[#14B05D] w-[40px] h-[30px]" />
         <span className="ml-2 text-[48px] leading-[57.6px]">WALLS</span>
       </div>
-      <div className="flex items-center space-x-6 p-6">
-        {wallsImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={wallsImage} />
       <SectionsHeading>SINKS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {sinksImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={sinksImage}/>
       <SectionsHeading>FAUCETS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {faucetsImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={faucetsImage} />
       <SectionsHeading>KITCHENS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {kitchensImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={kitchensImage} />
       <CategoryHeading>LANDSCAPING DEPARTMENT</CategoryHeading>
       <SectionsHeading>SHARUBS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {sharubsImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={sharubsImage} />
       <SectionsHeading>SHARUBS</SectionsHeading>
-      <div className="flex items-center space-x-6 p-6">
-        {pavementsImage.map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            width={471}
-            height={427}
-          />
-        ))}
-      </div>
+      <FeatureImages imagesName={pavementsImage} />
     </div>
   );
 }
---
+++ src/components/FeatureImages.tsx
@@ -0,0 +1,29 @@
+import Image, { StaticImageData } from "next/image";
+
+interface ImageProps {
+  id: string;
+  src: StaticImageData;
+  alt: string;
+}
+
+interface Props {
+  imagesName: ImageProps[];
+}
+
+const FeatureImages = ({ imagesName }: Props) => {
+  return (
+    <div className="flex items-center space-x-6 p-6">
+      {imagesName.map((image) => (
+        <Image
+          key={image.id}
+          src={image.src}
+          alt={image.alt}
+          width={471}
+          height={427}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default FeatureImages;
