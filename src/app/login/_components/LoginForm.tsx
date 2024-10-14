28863910e3533e64b9d0e47d3c40509e77ff4651
Zakaria Asad <zasad2587@gmail.com>
2024-10-14 16:13:59 +0300

    Refactor: extract the Login form component

---
+++ src/app/login/_components/LoginForm.tsx
@@ -0,0 +1,52 @@
+import StyledInput from '@/components/StyledInput'
+import Link from 'next/link'
+
+const LoginForm = () => {
+  return (
+    <form className="flex flex-col space-y-3 mt-12">
+          <label className="font-medium text-[#121C17] leading-[24.2px]">
+            EMAIL ADDRESS
+          </label>
+          <StyledInput
+            placeholder="Example@gmail.com"
+            type="email"
+            inputType="form-input"
+          />
+          <label className="font-medium text-[#121C17] leading-[24.2px]">
+            PASSWORD
+          </label>
+          <StyledInput
+            placeholder="**********"
+            type="password"
+            inputType="form-input"
+          />
+          <div className="flex items-center w-[478px] mt-2">
+            <span className="uppercase font-medium text-[15px] leading-[18.15px]">
+              don’t have an account?
+            </span>
+            <Link
+              href="/signup"
+              className="text-[#0E7E83] text-[15px] font-medium leading-[18.15px] ml-5"
+            >
+              SIGN UP
+            </Link>
+          </div>
+          <div className="flex items-center w-[478px] mt-2">
+            <span className="uppercase font-medium text-[15px] leading-[18.15px]">
+              forget password?
+            </span>
+            <Link
+              href="/signup"
+              className="text-[#0E7E83] text-[15px] font-medium leading-[18.15px] ml-5"
+            >
+              CHANGE PASSWORD
+            </Link>
+          </div>
+          <button className="bg-[#2BE784] text-[#121C17] font-medium rounded-lg w-[204px] h-[49px] mx-auto flex justify-center items-center">
+            LOGIN
+          </button>
+        </form>
+  )
+}
+
+export default LoginForm
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,7 @@
-import StyledInput from "@/components/StyledInput";
 import Image from "next/image";
-import Link from "next/link";
 import loginImage from "../../../public/Rectangle 11.png";
 import logo from "../../../public/logo.png";
+import LoginForm from "./_components/LoginForm";
 
 const LoginPage = () => {
   return (
@@ -23,49 +22,7 @@ const LoginPage = () => {
             className="mt-6"
           />
         </div>
-        <form className="flex flex-col space-y-3 mt-12">
-          <label className="font-medium text-[#121C17] leading-[24.2px]">
-            EMAIL ADDRESS
-          </label>
-          <StyledInput
-            placeholder="Example@gmail.com"
-            type="email"
-            inputType="form-input"
-          />
-          <label className="font-medium text-[#121C17] leading-[24.2px]">
-            PASSWORD
-          </label>
-          <StyledInput
-            placeholder="**********"
-            type="password"
-            inputType="form-input"
-          />
-          <div className="flex items-center w-[478px] mt-2">
-            <span className="uppercase font-medium text-[15px] leading-[18.15px]">
-              don’t have an account?
-            </span>
-            <Link
-              href="/signup"
-              className="text-[#0E7E83] text-[15px] font-medium leading-[18.15px] ml-5"
-            >
-              SIGN UP
-            </Link>
-          </div>
-          <div className="flex items-center w-[478px] mt-2">
-            <span className="uppercase font-medium text-[15px] leading-[18.15px]">
-              forget password?
-            </span>
-            <Link
-              href="/signup"
-              className="text-[#0E7E83] text-[15px] font-medium leading-[18.15px] ml-5"
-            >
-              CHANGE PASSWORD
-            </Link>
-          </div>
-          <button className="bg-[#2BE784] text-[#121C17] font-medium rounded-lg w-[204px] h-[49px] mx-auto flex justify-center items-center">
-            LOGIN
-          </button>
-        </form>
+        <LoginForm />
       </div>
       <div>
         <Image src={loginImage} alt="Login Image" width={970} height={879} />
