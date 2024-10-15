e246103ab603508cb6ac613c415e8d270e7fd9f7
Zakaria Asad <zasad2587@gmail.com>
2024-10-15 17:56:13 +0300

    Make the sginup page responsive

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,9 +7,14 @@ import SignupForm from "./_components/SignupForm";
 
 const SignUpPage = () => {
   return (
-    <div className="grid grid-cols-2">
-      <div className="relative">
-        <Image src={signupImage} alt="Signup Image" className="h-full" />
+    <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+      <div className="relative hidden lg:block">
+        <Image
+          src={signupImage}
+          alt="Signup Image"
+          layout="fill"
+          objectFit="cover"
+        />
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
           <Image src={eclipse} alt="logo" className="w-1/2 h-auto" />
         </div>
@@ -18,12 +23,12 @@ const SignUpPage = () => {
         </div>
       </div>
 
-      <div className="bg-[#EEF9F3] flex items-center flex-col h-full">
+      <div className="bg-[#EEF9F3] flex flex-col items-center justify-center h-full p-6">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-[#121C17] text-[48px] font-bold leading-[57.6px]">
+          <h1 className="text-[#121C17] text-[48px] font-bold leading-[57.6px] text-center">
             SIGN UP
           </h1>
-          <div className="relative">
+          <div className="relative mt-6">
             <Image
               src={eclipse}
               alt="profile picture"
