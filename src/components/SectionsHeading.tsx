65c9313bd524cc5497954e7cd5e95f51a7cc9a5d
Zakaria Asad <zasad2587@gmail.com>
2024-10-16 15:17:46 +0300

    Make SectionsHeading responsive

--- src/components/SectionsHeading.tsx
+++ src/components/SectionsHeading.tsx
@@ -8,14 +8,21 @@ interface Props {
 
 const SectionsHeading = ({ children, featureId }: Props) => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex flex-col md:flex-row justify-between items-center">
       <div className="flex mt-5 items-center">
         <div className="bg-[#14B05D] w-[40px] h-[30px]" />
-        <span className="ml-2 text-[48px] leading-[57.6px]">{children}</span>
+        <span
+          className="ml-2 lg:text-[48px] lg:leading-[57.6px] 
+          sm:text-[36px] sm:leading-[43.2px] 
+          text-[28px] leading-[34px]"
+        >
+          {children}
+        </span>
       </div>
       <Link
         href={`/features/${featureId}`}
-        className="w-[260px] h-[73px] flex items-center justify-center bg-[#074143] mr-5 mt-5 rounded-lg text-[#EEF9F3] text-[28px] leading-[33.89px] font-medium"
+        className="w-[260px] h-[73px] flex items-center justify-center bg-[#074143] mr-5 mt-5 rounded-lg text-[#EEF9F3] text-[28px] leading-[33.89px] font-medium
+          sm:w-[200px] sm:h-[60px] sm:text-[24px] sm:leading-[29px]"
       >
         VIEW ALL
       </Link>
