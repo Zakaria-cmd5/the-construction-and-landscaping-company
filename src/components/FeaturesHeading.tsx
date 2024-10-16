5b8228546b8465c505e13ad803746ca63166722d
Zakaria Asad <zasad2587@gmail.com>
2024-10-16 15:14:07 +0300

    Make the FeaturesHeading responsive

--- src/components/FeaturesHeading.tsx
+++ src/components/FeaturesHeading.tsx
@@ -10,9 +10,15 @@ const FeaturesHeading = ({ children, featureColor }: Props) => {
     <section
       className={`${
         featureColor === "feature-image" ? "bg-[#59ED9F]" : "bg-[#0E7E83]"
-      } bg-opacity-50 h-[188px] w-[1920px] flex items-center`}
+      } bg-opacity-50 h-[188px] w-[1920px] flex items-center 
+      sm:w-full sm:h-[150px]`} 
     >
-      <span className="text-[64px] p-4 leading-[76.8px]">{children}</span>
+      <span
+        className="lg:text-[64px] p-4 lg:leading-[76.8px] 
+      sm:text-[48px] sm:leading-[60px] text-[32px] leading-[40px]" 
+      >
+        {children}
+      </span>
     </section>
   );
 };
