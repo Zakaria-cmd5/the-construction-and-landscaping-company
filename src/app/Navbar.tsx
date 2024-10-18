ef6d331edadee196c6a2e9929f38ece137918521
Zakaria Asad <zasad2587@gmail.com>
2024-10-18 14:37:14 +0300

    Refactor: Extract the desktop navbar and  update the text size on the mobile navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,23 +1,16 @@
 "use client";
 
 import Search from "@/components/Search";
-import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 import iconImage from "../../public/logo.png";
+import DesktopNavbar from "./_components/DesktopNavbar";
 
 const Navbar = () => {
-  const currentPath = usePathname();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isDesktopMenuOpen, setIsDesktopMenuOpen] = useState(false);
 
-  const links = [
-    { href: "/", label: "HOME" },
-    { href: "/about-us", label: "ABOUT US" },
-    { href: "/projects", label: "PROJECTS" },
-  ];
+ 
 
   const mobileLinks = [
     { href: "/", label: "HOME" },
@@ -26,96 +19,12 @@ const Navbar = () => {
     { href: "/profile", label: "PROFILE" },
   ];
 
-  const menuLinks = [
-    { href: "/", label: "landscaping" },
-    { href: "/", label: "decking" },
-    { href: "/", label: "gardening" },
-    { href: "/", label: "interlocking" },
-    { href: "/", label: "floral design" },
-    { href: "/", label: "pools" },
-    { href: "/", label: "railings" },
-    { href: "/", label: "woodwoorking" },
-    { href: "/", label: "showroom" },
-    { href: "/profile", label: "profile" },
-    { href: "/", label: "contact us" },
-  ];
+
 
   return (
     <>
       {/* Desktop Navbar */}
-      <div className="hidden md:flex bg-[#0E7E83] justify-between items-center w-full h-[79px]">
-        <Image
-          src={iconImage}
-          alt="icon"
-          width={69}
-          height={57}
-          className="relative -top-1"
-        />
-        <p className="text-[#eef6f3] text-[20px] text-center leading-[24.2px] w-[287px] h-[48px]">
-          THE CONSTRUCTION AND LANDSCAPING COMPANY
-        </p>
-        <div className="w-[80px] border-2 bg-[#EEF9F3] -rotate-90" />
-        <ul className="flex items-center">
-          {links.map((link) => (
-            <li
-              className={`h-[49px] ${
-                link.label === "HOME"
-                  ? "w-[88px] "
-                  : link.label === "ABOUT US"
-                  ? "w-[120px]"
-                  : link.label === "PROJECTS"
-                  ? "w-[152px]"
-                  : ""
-              } flex items-center justify-center`}
-              key={link.href}
-            >
-              <Link
-                href={link.href}
-                className={classNames({
-                  "text-[#eef6f3] font-bold": true,
-                  "border-b-2": link.href === currentPath,
-                })}
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Search />
-        <div className="flex items-center">
-          <div
-            className="h-[59px] w-[59px] flex flex-col justify-center items-center space-y-1"
-            onClick={() => {
-              setIsDesktopMenuOpen((prev) => !prev);
-            }}
-          >
-            <span className="bg-[#EEF9F3] w-[44.25px] h-[3.69px] rounded" />
-            <span className="bg-[#EEF9F3] w-[36.88px] h-[3.69px] rounded -ml-2" />
-            <span className="bg-[#EEF9F3] w-[44.25px] h-[3.69px] rounded" />
-          </div>
-          <span className="text-[#eef6f3]">MENU</span>
-        </div>
-        <Link href='/signup' className="p-4 text-[#EEF9F3] bg-[#59ED9F] mr-6 rounded-lg border-4 border-[#EEF9F3]">
-          BOOK NOW
-        </Link>
-        {isDesktopMenuOpen && (
-          <div className="w-[400px] h-[500px] bg-[#EEF9F3] absolute z-10 top-20 left-[58%] rounded-l-lg mt-1">
-            <ul className="flex flex-col items-center justify-center space-x-2 uppercase">
-              {menuLinks.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    onClick={() => setIsDesktopMenuOpen(false)}
-                    href={link.href}
-                    className="text-[#121C17] hover:text-[#0E7E83] font-medium leading-[43.57px]"
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+      <DesktopNavbar />
 
       {/* Mobile Navbar */}
       <div className="md:hidden bg-[#0E7E83] flex justify-between items-center px-4 py-2">
@@ -126,7 +35,7 @@ const Navbar = () => {
           height={40}
           className="relative -top-1"
         />
-        <p className="text-[#eef6f3] text-[16px] leading-[20px]">
+        <p className="text-[#eef6f3] lg:text-[16px] text-[14px] lg:leading-[20px]">
           THE CONSTRUCTION AND LANDSCAPING COMPANY
         </p>
         <button
---
+++ src/app/_components/DesktopNavbar.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import Search from "@/components/Search";
+import classNames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import iconImage from "../../../public/logo.png";
+
+const DesktopNavbar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { href: "/", label: "HOME" },
+    { href: "/about-us", label: "ABOUT US" },
+    { href: "/projects", label: "PROJECTS" },
+  ];
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuLinks = [
+    { href: "/", label: "landscaping" },
+    { href: "/", label: "decking" },
+    { href: "/", label: "gardening" },
+    { href: "/", label: "interlocking" },
+    { href: "/", label: "floral design" },
+    { href: "/", label: "pools" },
+    { href: "/", label: "railings" },
+    { href: "/", label: "woodwoorking" },
+    { href: "/", label: "showroom" },
+    { href: "/profile", label: "profile" },
+    { href: "/", label: "contact us" },
+  ];
+
+  return (
+    <div className="hidden md:flex bg-[#0E7E83] justify-between items-center w-full h-[79px]">
+      <Image
+        src={iconImage}
+        alt="icon"
+        width={69}
+        height={57}
+        className="relative -top-1"
+      />
+      <p className="text-[#eef6f3] text-[20px] text-center leading-[24.2px] w-[287px] h-[48px]">
+        THE CONSTRUCTION AND LANDSCAPING COMPANY
+      </p>
+      <div className="w-[80px] border-2 bg-[#EEF9F3] -rotate-90" />
+      <ul className="flex items-center">
+        {links.map((link) => (
+          <li
+            className={`h-[49px] ${
+              link.label === "HOME"
+                ? "w-[88px] "
+                : link.label === "ABOUT US"
+                ? "w-[120px]"
+                : link.label === "PROJECTS"
+                ? "w-[152px]"
+                : ""
+            } flex items-center justify-center`}
+            key={link.href}
+          >
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-[#eef6f3] font-bold": true,
+                "border-b-2": link.href === currentPath,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Search />
+      <div className="flex items-center">
+        <div
+          className="h-[59px] w-[59px] flex flex-col justify-center items-center space-y-1"
+          onClick={() => {
+            setIsMenuOpen((prev) => !prev);
+          }}
+        >
+          <span className="bg-[#EEF9F3] w-[44.25px] h-[3.69px] rounded" />
+          <span className="bg-[#EEF9F3] w-[36.88px] h-[3.69px] rounded -ml-2" />
+          <span className="bg-[#EEF9F3] w-[44.25px] h-[3.69px] rounded" />
+        </div>
+        <span className="text-[#eef6f3]">MENU</span>
+      </div>
+      <Link
+        href="/signup"
+        className="p-4 text-[#EEF9F3] bg-[#59ED9F] mr-6 rounded-lg border-4 border-[#EEF9F3]"
+      >
+        BOOK NOW
+      </Link>
+      {isMenuOpen && (
+        <div className="w-[400px] h-[500px] bg-[#EEF9F3] absolute z-10 top-20 left-[62%] rounded-l-lg mt-1">
+          <ul className="flex flex-col items-center justify-center space-x-2 uppercase">
+            {menuLinks.map((link) => (
+              <li key={link.href}>
+                <Link
+                  onClick={() => setIsMenuOpen(false)}
+                  href={link.href}
+                  className="text-[#121C17] hover:text-[#0E7E83] font-medium leading-[43.57px]"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DesktopNavbar;
