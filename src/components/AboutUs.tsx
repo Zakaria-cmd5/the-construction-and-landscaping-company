bf12fcc41acc354d5fa12fe70d4d8221ede03617
Zakaria Asad <zasad2587@gmail.com>
2024-10-20 23:13:31 +0300

    Translate the aboutUs component

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,44 +1,43 @@
+"use client";
+
+import "@/utils/i18n";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 const AboutUs = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(navigator.language);
+  }, [i18n]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 mt-16 p-4 gap-4">
       <div className="w-full uppercase text-[#031C1D]">
         <p className="text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-          Welcome to Z and N Construction and Landscaping, where quality meets
-          creativity. Founded with a passion for transforming outdoor spaces and
-          building lasting structures, we are dedicated to providing exceptional
-          services that enhance both the beauty and functionality of your
-          property.
+          {t("aboutUsPageIntroductionAboutTheCompany")}
         </p>
         <h2 className="mt-10 font-bold text-[24px] md:text-[28px]">
-          Our Mission
+          {t("aboutUsPageMissionOfTheCompanyHeading")}
         </h2>
         <p className="text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-          At Z and N, our mission is to deliver high-quality construction and
-          landscaping solutions tailored to meet the unique needs of our
-          clients. We believe in creating spaces that reflect your vision while
-          ensuring durability and sustainability.
-        </p>
-        <h2 className="mt-10 font-bold text-[24px] md:text-[28px]">Our Team</h2>
-        <p className="text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-          Our experienced team of professionals brings a wealth of knowledge and
-          expertise to every project. From skilled landscapers to expert
-          builders, we work collaboratively to ensure that each job is completed
-          to the highest standards. We pride ourselves on our attention to
-          detail, commitment to excellence, and passion for our craft.
+          {t("aboutUsPageMissionOfTheCompanyDescription")}
         </p>
         <h2 className="mt-10 font-bold text-[24px] md:text-[28px]">
-          What We Offer
+          {t("aboutUsPageTeamOfTheCompanyHeading")}
+        </h2>
+        <p className="text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
+          {t("aboutUsPageTeamOfTheCompanyDescription")}
+        </p>
+        <h2 className="mt-10 font-bold text-[24px] md:text-[28px]">
+          {t("aboutUsPageCompanyOffersHeading")}
         </h2>
         <ul className="list-disc pl-8">
           <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-            Construction Services: From residential projects to commercial
-            builds, we handle everything from design to execution, ensuring a
-            seamless construction process.
+          {t("aboutUsPageCompanyOffersDescriptionFirstLi")}
           </li>
           <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-            Landscaping Services: Our landscaping team specializes in creating
-            beautiful gardens, patios, and outdoor living spaces that enhance
-            your property’s appeal.
+          {t("aboutUsPageCompanyOffersDescriptionSecondLi")}
           </li>
         </ul>
       </div>
@@ -46,26 +45,20 @@ const AboutUs = () => {
         {/* Here we will put a video later on */}
         <div className="w-full h-[300px] md:w-[852px] md:h-[587px] bg-black mb-4"></div>
 
-        <h2 className="font-bold text-[24px] md:text-[28px]">Why Choose Us?</h2>
+        <h2 className="font-bold text-[24px] md:text-[28px]">{t("aboutUsPageWhyChooseUsHeading")}</h2>
         <ul className="list-disc pl-8">
           <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-            Quality Craftsmanship: We use only the best materials and techniques
-            to ensure lasting results.
+          {t("aboutUsPageWhyChooseUsDescriptionFirstLi")}
           </li>
           <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-            Customer-Centric Approach: Your satisfaction is our priority. We
-            listen to your needs and work closely with you throughout the
-            project.
+          {t("aboutUsPageWhyChooseUsDescriptionSecondLi")}
           </li>
-          <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-            Sustainable Practices: We are committed to environmentally friendly
-            practices that benefit both our clients and the planet.
+          <li className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">            
+          {t("aboutUsPageWhyChooseUsDescriptionThirdLi")}
           </li>
         </ul>
         <p className="mt-2 text-[24px] leading-[29px] font-medium md:text-[28px] md:leading-[33.89px]">
-          Join us in creating beautiful, functional spaces that you can cherish
-          for years to come. At Z and N Construction and Landscaping, we turn
-          your dreams into reality.
+          {t("aboutUsPageJoinUsDescription")}
         </p>
       </div>
     </div>
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -6,19 +6,191 @@ i18n.use(initReactI18next).init({
   fallbackLng: "en",
   resources: {
     en: {
-      translation: {},
+      translation: {
+        aboutUsPageIntroductionAboutTheCompany:
+          "Welcome to Z and N Construction and Landscaping, where quality meets creativity. Founded with a passion for transforming outdoor spaces and building lasting structures, we are dedicated to providing exceptional services that enhance both the beauty and functionality of your property.",
+
+        aboutUsPageMissionOfTheCompanyHeading: "Our Mission",
+
+        aboutUsPageMissionOfTheCompanyDescription:
+          "At Z and N, our mission is to deliver high-quality construction and landscaping solutions tailored to meet the unique needs of our clients. We believe in creating spaces that reflect your vision while ensuring durability and sustainability.",
+
+        aboutUsPageTeamOfTheCompanyHeading: "Our Team",
+
+        aboutUsPageTeamOfTheCompanyDescription:
+          "Our experienced team of professionals brings a wealth of knowledge and expertise to every project. From skilled landscapers to expert builders, we work collaboratively to ensure that each job is completed to the highest standards. We pride ourselves on our attention to detail, commitment to excellence, and passion for our craft.",
+
+        aboutUsPageCompanyOffersHeading: "What We Offer",
+
+        aboutUsPageCompanyOffersDescriptionFirstLi:
+          "Construction Services: From residential projects to commercial builds, we handle everything from design to execution, ensuring a seamless construction process.",
+
+        aboutUsPageCompanyOffersDescriptionSecondLi:
+          "Landscaping Services: Our landscaping team specializes in creating beautiful gardens, patios, and outdoor living spaces that enhance your property’s appeal.",
+
+        aboutUsPageWhyChooseUsHeading: "Why Choose Us?",
+
+        aboutUsPageWhyChooseUsDescriptionFirstLi:
+          "Quality Craftsmanship: We use only the best materials and techniques to ensure lasting results.",
+
+        aboutUsPageWhyChooseUsDescriptionSecondLi:
+          "Customer-Centric Approach: Your satisfaction is our priority. Welisten to your needs and work closely with you throughout theproject.",
+
+        aboutUsPageWhyChooseUsDescriptionThirdLi:
+          "Sustainable Practices: We are committed to environmentally friendly practices that benefit both our clients and the planet.",
+
+        aboutUsPageJoinUsDescription:
+          "Join us in creating beautiful, functional spaces that you can cherish for years to come. At Z and N Construction and Landscaping, we turn your dreams into reality.",
+      },
     },
     ar: {
-      translation: {},
+      translation: {
+        aboutUsPageIntroductionAboutTheCompany:
+          "مرحبًا بكم في Z and N Construction and Landscaping، حيث تلتقي الجودة إِبداع. تأسست بشغف لتحويل المساحات الخارجية و بناء هياكل دائمة، ونحن ملتزمون بتقديم استثنائية الخدمات التي تعزز الجمال والأداء الوظيفي الخاص بك ملكية.",
+
+        aboutUsPageMissionOfTheCompanyHeading: "مهمتنا",
+
+        aboutUsPageMissionOfTheCompanyDescription:
+          "في Z and N، مهمتنا هي تقديم بناء عالي الجودة و حلول تنسيق الحدائق مصممة لتلبية الاحتياجات الفريدة لدينا عملاء. نحن نؤمن بأهمية خلق مساحات تعكس رؤيتك ضمان المتانة والاستدامة.",
+
+        aboutUsPageTeamOfTheCompanyHeading: "فريقنا",
+
+        aboutUsPageTeamOfTheCompanyDescription:
+          "لدينا فريق من المهنيين ذوي الخبرة يجلب ثروة من المعرفة والخبرة لكل مشروع. من تنسيق الحدائق المهرة إلى الخبراء البناة، نحن نعمل بشكل تعاوني لضمان اكتمال كل مهمة بأعلى المعايير. نحن نفخر باهتمامنا به التفاصيل والالتزام بالتميز والشغف بحرفتنا.",
+
+        aboutUsPageCompanyOffersHeading: "ما نقدمه",
+
+        aboutUsPageCompanyOffersDescriptionFirstLi:
+          "خدمات البناء: من المشاريع السكنية إلى التجارية نحن نتعامل مع كل شيء بدءًا من التصميم وحتى التنفيذ، مما يضمن عملية بناء سلسة.",
+
+        aboutUsPageCompanyOffersDescriptionSecondLi:
+          "خدمات تنسيق الحدائق: فريق تنسيق الحدائق لدينا متخصص في التصميم الحدائق الجميلة والباحات ومساحات المعيشة الخارجية التي تعزز جاذبية الممتلكات الخاصة بك.",
+
+        aboutUsPageWhyChooseUsHeading: "لماذا تختارنا؟",
+
+        aboutUsPageWhyChooseUsDescriptionFirstLi:
+          "جودة الصناعة: نحن نستخدم فقط أفضل المواد والتقنيات لضمان نتائج دائمة.",
+
+        aboutUsPageWhyChooseUsDescriptionSecondLi:
+          "نهج يركز على العملاء: رضاكم هو أولويتنا. نستمع إلى احتياجاتك ونعمل معك بشكل وثيق طوال المشروع.",
+
+        aboutUsPageWhyChooseUsDescriptionThirdLi:
+          "الممارسات المستدامة: نحن ملتزمون بالممارسات الصديقة للبيئة التي تعود بالنفع على عملائنا والكوكب.",
+
+        aboutUsPageJoinUsDescription:
+          "انضم إلينا في إنشاء مساحات جميلة وعملية يمكنك الاعتزاز بها لسنوات قادمة. في Z and N Construction and Landscaping، نحول أحلامك إلى حقيقة.",
+      },
     },
     de: {
-      translation: {},
+      translation: {
+        aboutUsPageIntroductionAboutTheCompany:
+          "Willkommen bei Z and N Construction and Landscaping, wo Qualität aufeinander trifft Kreativität. Gegründet mit einer Leidenschaft für die Umgestaltung von Außenräumen und Wir bauen dauerhafte Strukturen und sind bestrebt, Außergewöhnliches zu bieten",
+
+        aboutUsPageMissionOfTheCompanyHeading: "Unsere Mission",
+
+        aboutUsPageMissionOfTheCompanyDescription:
+          "Bei Z and N ist es unsere Mission, qualitativ hochwertige Konstruktionen zu liefern Landschaftsbaulösungen, die auf die individuellen Bedürfnisse unserer Kunden zugeschnitten sind Kunden. Wir glauben daran, Räume zu schaffen, die Ihre Vision widerspiegeln Gewährleistung von Haltbarkeit und Nachhaltigkeit.",
+
+        aboutUsPageTeamOfTheCompanyHeading: "Unser Team",
+
+        aboutUsPageTeamOfTheCompanyDescription:
+          "Unser erfahrenes Team von Fachleuten bringt eine Fülle von Kenntnissen mit Fachkompetenz für jedes Projekt. Vom erfahrenen Landschaftsgärtner zum Experten Als Bauherren arbeiten wir eng zusammen, um sicherzustellen, dass jeder Auftrag abgeschlossen wird auf höchstem Niveau. Wir sind stolz auf unsere Aufmerksamkeit Liebe zum Detail, Engagement für Exzellenz und Leidenschaft für unser Handwerk.",
+
+        aboutUsPageCompanyOffersHeading: "Was wir anbieten",
+
+        aboutUsPageCompanyOffersDescriptionFirstLi:
+          "Baudienstleistungen: Von Wohnprojekten bis hin zu Gewerbeprojekten Builds kümmern wir uns um alles vom Entwurf bis zur Ausführung und stellen sicher, dass ein reibungsloser Bauablauf.",
+
+        aboutUsPageCompanyOffersDescriptionSecondLi:
+          "Landschaftsbau-Dienstleistungen: Unser Landschaftsbau-Team ist auf die Erstellung spezialisiert wunderschöne Gärten, Terrassen und Wohnräume im Freien, die aufwerten die Attraktivität Ihrer Immobilie.",
+
+        aboutUsPageWhyChooseUsHeading: "Warum uns wählen?",
+
+        aboutUsPageWhyChooseUsDescriptionFirstLi:
+          "Hochwertige Handwerkskunst: Wir verwenden nur die besten Materialien und Techniken, um dauerhafte Ergebnisse zu gewährleisten.",
+
+        aboutUsPageWhyChooseUsDescriptionSecondLi:
+          "Kundenzentrierter Ansatz: Ihre Zufriedenheit ist unsere Priorität. Wir gehen auf Ihre Bedürfnisse ein und arbeiten während des gesamten Projekts eng mit Ihnen zusammen.",
+
+        aboutUsPageWhyChooseUsDescriptionThirdLi:
+          "Nachhaltige Praktiken: Wir verpflichten uns zu umweltfreundlichen Praktiken, die sowohl unseren Kunden als auch dem Planeten zugute kommen.",
+
+        aboutUsPageJoinUsDescription:
+          "Schaffen Sie mit uns schöne, funktionale Räume, an denen Sie noch viele Jahre Freude haben werden. Bei Z and N Construction and Landscaping verwirklichen wir Ihre Träume.",
+      },
     },
-    jp: {
-      translation: {},
+    ja: {
+      translation: {
+        aboutUsPageIntroductionAboutTheCompany:
+          "品質と品質が出会うZ and N Construction and Landscape へようこそ 創造性。屋外空間を変革するという情熱を持って設立され、 永続的な構造を構築するために、私たちは優れた製品を提供することに専念しています 美しさと機能性の両方を高めるサービス 財産。",
+
+        aboutUsPageMissionOfTheCompanyHeading: "私たちの使命",
+
+        aboutUsPageMissionOfTheCompanyDescription:
+          "ゼットアンドエヌの使命は、高品質な施工と施工をお届けすることです。 私たちの固有のニーズを満たすように調整された造園ソリューション クライアント。私たちは、お客様のビジョンを反映した空間を創造することを信じています。 耐久性と持続可能性を確保します。",
+
+        aboutUsPageTeamOfTheCompanyHeading: "私たちのチーム",
+
+        aboutUsPageTeamOfTheCompanyDescription:
+          "当社の経験豊富な専門家チームは豊富な知識と経験をもたらします。 あらゆるプロジェクトに専門知識を提供します。熟練の造園家からエキスパートまで 建設業者の皆様、私たちは各仕事が確実に完了するよう協力して作業します 最高の基準に。私たちは次のことに誇りを持っています。 細部へのこだわり、卓越性へのこだわり、そして私たちのものづくりへの情熱。",
+
+        aboutUsPageCompanyOffersHeading: "私たちが提供するもの",
+
+        aboutUsPageCompanyOffersDescriptionFirstLi:
+          "建設サービス: 住宅プロジェクトから商業プロジェクトまで 構築、設計から実行まですべてを担当し、 シームレスな建設プロセス。",
+
+        aboutUsPageCompanyOffersDescriptionSecondLi:
+          "造園サービス: 私たちの造園チームは造園を専門としています。 美しい庭園、パティオ、そして屋外のリビングスペースは、 あなたの物件の魅力。",
+
+        aboutUsPageWhyChooseUsHeading: "当社を選ぶ理由",
+
+        aboutUsPageWhyChooseUsDescriptionFirstLi:
+          "高品質の職人技: 永続的な結果を保証するために、最高の素材と技術のみを使用しています。",
+
+        aboutUsPageWhyChooseUsDescriptionSecondLi:
+          "顧客中心のアプローチ: お客様の満足が当社の最優先事項です。お客様のニーズに耳を傾け、プロジェクト全体を通じて緊密に連携します。",
+
+        aboutUsPageWhyChooseUsDescriptionThirdLi:
+          "持続可能な実践: 私たちは、クライアントと地球の両方に利益をもたらす環境に優しい実践に取り組んでいます。",
+
+        aboutUsPageJoinUsDescription:
+          "長く大切にしていただける美しく機能的な空間づくりに、私たちと一緒に取り組んでいきましょう。 Z and N Construction and Landscaping では、お客様の夢を現実にします。",
+      },
     },
     es: {
-      translation: {},
+      translation: {
+        aboutUsPageIntroductionAboutTheCompany:
+          "Bonvenon al Z kaj N Konstruado kaj Pejzaĝigado, kie kvalito renkontiĝas kreivo. Fondita kun pasio por transformi subĉielajn spacojn kaj konstruante daŭrajn strukturojn, ni dediĉas al provizi esceptajn servoj kiuj plibonigas kaj la belecon kaj funkciecon de via posedaĵo.",
+
+        aboutUsPageMissionOfTheCompanyHeading: "Nia Misio",
+
+        aboutUsPageMissionOfTheCompanyDescription:
+          "Ĉe Z kaj N, nia misio estas liveri altkvalitan konstruadon kaj pejzaĝaj solvoj adaptitaj por renkonti la unikajn bezonojn de nia klientoj. Ni kredas je kreado de spacoj kiuj reflektas vian vizion dum certigante fortikecon kaj daŭripovon.",
+
+        aboutUsPageTeamOfTheCompanyHeading: `Nia Teamo`,
+
+        aboutUsPageTeamOfTheCompanyDescription: `Nia sperta teamo de profesiuloj alportas riĉecon da scio kaj kompetenteco al ĉiu projekto. De lertaj pejzaĝistoj ĝis spertaj konstruistoj, ni kunlaboras por certigi, ke ĉiu laboro estas finita al la plej altaj normoj. Ni fieras pri nia atento detalo, engaĝiĝo al plejboneco kaj pasio por nia metio.`,
+
+        aboutUsPageCompanyOffersHeading: `Kion Ni Ofertas`,
+
+        aboutUsPageCompanyOffersDescriptionFirstLi: `Konstruaj Servoj: De loĝprojektoj ĝis komercaj konstruas, ni pritraktas ĉion de dezajno ĝis ekzekuto, certigante a senjunta konstruprocezo.`,
+
+        aboutUsPageCompanyOffersDescriptionSecondLi: `Pejzaĝaj Servoj: Nia pejzaĝa teamo specialiĝas pri kreado belaj ĝardenoj, kortoj kaj subĉielaj loĝejoj, kiuj plifortigas la alogo de via posedaĵo.`,
+
+        aboutUsPageWhyChooseUsHeading: "Kial Elekti Nin?",
+
+        aboutUsPageWhyChooseUsDescriptionFirstLi:
+          "Kvalita Metiado: Ni uzas nur la plej bonajn materialojn kaj teknikojn por certigi daŭrajn rezultojn.",
+
+        aboutUsPageWhyChooseUsDescriptionSecondLi:
+          "Kliento-Centra Aliro: Via kontento estas nia prioritato. Bonvolu aŭskulti viajn bezonojn kaj labori proksime kun vi dum la projekto.",
+
+        aboutUsPageWhyChooseUsDescriptionThirdLi:
+          "Daŭrigeblaj Praktikoj: Ni estas engaĝitaj al ekologiemaj praktikoj, kiuj profitigas kaj niajn klientojn kaj la planedon.",
+
+        aboutUsPageJoinUsDescription:
+          "Aliĝu al ni por krei belajn, funkciajn spacojn, kiujn vi povas ŝati dum venontaj jaroj. Ĉe Z kaj N Konstruo kaj Pejzaĝigado, ni transformas viajn revojn en realaĵon.",
+      },
     },
   },
 });
