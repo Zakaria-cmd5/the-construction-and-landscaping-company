1a6326d5fe2ffd657dda5c9df8345d5421ae0c32
Zakaria Asad <zasad2587@gmail.com>
2024-10-22 06:01:12 +0300

    Refactor: use classNames library on the FeaturesHeading component

--- src/components/FeaturesHeading.tsx
+++ src/components/FeaturesHeading.tsx
@@ -1,4 +1,5 @@
 import { useDarkMode } from "@/context/DarkModeToggleProvider";
+import classNames from "classnames";
 import { ReactNode } from "react";
 
 interface Props {
@@ -9,21 +10,22 @@ interface Props {
 const FeaturesHeading = ({ children, featureColor }: Props) => {
   const { darkMode } = useDarkMode();
 
+  const sectionClassName = classNames({
+    "bg-opacity-50 h-[188px] w-[1920px] flex items-center sm:w-full sm:h-[150px]":
+      true,
+    "bg-[#59ED9F]": featureColor === "feature-image",
+    "bg-[#0E7E83]": featureColor === "feature-explain",
+  });
+
+  const spanClassName = classNames({
+    "lg:text-[64px] p-4 lg:leading-[76.8px] sm:text-[48px] sm:leading-[60px] text-[32px] leading-[40px]":
+      true,
+    "text-white": darkMode,
+  });
+
   return (
-    <section
-      className={`${
-        featureColor === "feature-image" ? "bg-[#59ED9F]" : "bg-[#0E7E83]"
-      } bg-opacity-50 h-[188px] w-[1920px] flex items-center 
-      sm:w-full sm:h-[150px]`}
-    >
-      <span
-        className={`lg:text-[64px] p-4 lg:leading-[76.8px] 
-      sm:text-[48px] sm:leading-[60px] text-[32px] leading-[40px] ${
-        darkMode && "text-white"
-      }`}
-      >
-        {children}
-      </span>
+    <section className={sectionClassName}>
+      <span className={spanClassName}>{children}</span>
     </section>
   );
 };
