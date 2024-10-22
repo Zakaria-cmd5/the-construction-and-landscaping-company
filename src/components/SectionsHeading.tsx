c0c82608afa9483f8afe6ad941ee9d0a0025d017
Zakaria Asad <zasad2587@gmail.com>
2024-10-22 12:13:51 +0300

    Refactor: Use classNames on the SectionsHeading component

--- src/components/SectionsHeading.tsx
+++ src/components/SectionsHeading.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useDarkMode } from "@/context/DarkModeToggleProvider";
+import classNames from "classnames";
 import Link from "next/link";
 import { ReactNode, useEffect } from "react";
 import { useTranslation } from "react-i18next";
@@ -11,7 +12,7 @@ interface Props {
 }
 
 const SectionsHeading = ({ children, featureId }: Props) => {
-  const {darkMode} = useDarkMode()
+  const { darkMode } = useDarkMode();
 
   const { t, i18n } = useTranslation();
 
@@ -24,9 +25,11 @@ const SectionsHeading = ({ children, featureId }: Props) => {
       <div className="flex mt-5 items-center">
         <div className="bg-[#14B05D] w-[40px] h-[30px]" />
         <span
-          className={`ml-2 lg:text-[48px] lg:leading-[57.6px] 
-          sm:text-[36px] sm:leading-[43.2px] 
-          text-[28px] leading-[34px] ${darkMode && 'text-white'}`}
+          className={classNames({
+            "ml-2 lg:text-[48px] lg:leading-[57.6px] sm:text-[36px] sm:leading-[43.2px] text-[28px] leading-[34px]":
+              true,
+            "text-white": darkMode,
+          })}
         >
           {children}
         </span>
@@ -36,7 +39,7 @@ const SectionsHeading = ({ children, featureId }: Props) => {
         className="w-[260px] h-[73px] flex items-center justify-center bg-[#074143] mr-5 mt-5 rounded-lg text-[#EEF9F3] text-[28px] leading-[33.89px] font-medium
           sm:w-[200px] sm:h-[60px] sm:text-[24px] sm:leading-[29px]"
       >
-        {t('SectionsHeadingButtonLabel')}
+        {t("SectionsHeadingButtonLabel")}
       </Link>
     </div>
   );
