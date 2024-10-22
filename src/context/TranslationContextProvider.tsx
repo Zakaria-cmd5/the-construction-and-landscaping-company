51922b5759a47df47d3d2e26f33188a55ee09746
Zakaria Asad <zasad2587@gmail.com>
2024-10-22 21:41:13 +0300

    Make the translate on the system Language

--- src/context/TranslationContextProvider.tsx
+++ src/context/TranslationContextProvider.tsx
@@ -1,6 +1,38 @@
+// Translate using browser Lang
+// "use client";
+
+// import { createContext, PropsWithChildren, useContext, useEffect } from "react";
+// import { useTranslation } from "react-i18next";
+
+// const TranslationContext = createContext({
+//   t: (key: string) => key,
+//   i18n: { changeLanguage: () => {} },
+// });
+
+// const TranslationContextProvider = ({ children }: PropsWithChildren) => {
+//   const { t, i18n } = useTranslation();
+
+//   useEffect(() => {
+//     i18n.changeLanguage(navigator.language);
+//   }, [i18n]);
+
+//   return (
+//     <TranslationContext.Provider value={{ t, i18n }}>
+//       {children}
+//     </TranslationContext.Provider>
+//   );
+// };
+
+// export default TranslationContextProvider;
+
+// export const useTranslationContext = () => {
+//   return useContext(TranslationContext);
+// };
+
+// Translate using system Lang
 "use client";
 
-import { createContext, PropsWithChildren, useContext, useEffect } from "react";
+import { createContext, PropsWithChildren, useContext } from "react";
 import { useTranslation } from "react-i18next";
 
 const TranslationContext = createContext({
@@ -11,9 +43,15 @@ const TranslationContext = createContext({
 const TranslationContextProvider = ({ children }: PropsWithChildren) => {
   const { t, i18n } = useTranslation();
 
-  useEffect(() => {
-    i18n.changeLanguage(navigator.language);
-  }, [i18n]);
+  if (typeof navigator !== "undefined") {
+    const userLanguage = navigator.languages
+      ? navigator.languages[0]
+      : navigator.language;
+
+    if (i18n.changeLanguage) {
+      i18n.changeLanguage(userLanguage);
+    }
+  }
 
   return (
     <TranslationContext.Provider value={{ t, i18n }}>
