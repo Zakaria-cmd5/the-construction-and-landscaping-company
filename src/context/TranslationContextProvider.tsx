a27db693c2d4d3b0c03798b055b30f64085f577a
Zakaria Asad <zasad2587@gmail.com>
2024-10-22 21:53:32 +0300

    Take the browser translate approch

--- src/context/TranslationContextProvider.tsx
+++ src/context/TranslationContextProvider.tsx
@@ -1,35 +1,4 @@
 // Translate using browser Lang
-// "use client";
-
-// import { createContext, PropsWithChildren, useContext, useEffect } from "react";
-// import { useTranslation } from "react-i18next";
-
-// const TranslationContext = createContext({
-//   t: (key: string) => key,
-//   i18n: { changeLanguage: () => {} },
-// });
-
-// const TranslationContextProvider = ({ children }: PropsWithChildren) => {
-//   const { t, i18n } = useTranslation();
-
-//   useEffect(() => {
-//     i18n.changeLanguage(navigator.language);
-//   }, [i18n]);
-
-//   return (
-//     <TranslationContext.Provider value={{ t, i18n }}>
-//       {children}
-//     </TranslationContext.Provider>
-//   );
-// };
-
-// export default TranslationContextProvider;
-
-// export const useTranslationContext = () => {
-//   return useContext(TranslationContext);
-// };
-
-// Translate using system Lang
 "use client";
 
 import { createContext, PropsWithChildren, useContext, useEffect } from "react";
@@ -44,10 +13,7 @@ const TranslationContextProvider = ({ children }: PropsWithChildren) => {
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    const userLanguage = navigator.languages
-      ? navigator.languages[0]
-      : navigator.language;
-    i18n.changeLanguage(userLanguage);
+    i18n.changeLanguage(navigator.language);
   }, [i18n]);
 
   return (
@@ -62,3 +28,37 @@ export default TranslationContextProvider;
 export const useTranslationContext = () => {
   return useContext(TranslationContext);
 };
+
+// Translate using system Lang
+// "use client";
+
+// import { createContext, PropsWithChildren, useContext, useEffect } from "react";
+// import { useTranslation } from "react-i18next";
+
+// const TranslationContext = createContext({
+//   t: (key: string) => key,
+//   i18n: { changeLanguage: () => {} },
+// });
+
+// const TranslationContextProvider = ({ children }: PropsWithChildren) => {
+//   const { t, i18n } = useTranslation();
+
+//   useEffect(() => {
+//     const userLanguage = navigator.languages
+//       ? navigator.languages[0]
+//       : navigator.language;
+//     i18n.changeLanguage(userLanguage);
+//   }, [i18n]);
+
+//   return (
+//     <TranslationContext.Provider value={{ t, i18n }}>
+//       {children}
+//     </TranslationContext.Provider>
+//   );
+// };
+
+// export default TranslationContextProvider;
+
+// export const useTranslationContext = () => {
+//   return useContext(TranslationContext);
+// };
