e86fb146e2a4726d6c21d7f857c95d8f70f96cb8
Zakaria Asad <zasad2587@gmail.com>
2024-10-24 23:40:34 +0200

    Build up the middle ware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "./lib/session";
+
+const protectedRoute = ["/profile", "/features"];
+const publicRoutes = ["/login", "/signup"];
+
+export async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoute.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookie = cookies().get("session")?.value;
+  const session = await decrypt(cookie);
+
+  if (!isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL("/signup", req.nextUrl));
+  }
+
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
