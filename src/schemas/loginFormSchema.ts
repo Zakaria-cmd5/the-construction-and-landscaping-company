f75d6487627d3ef425a0127ffa009ce1db6c81a2
Zakaria Asad <zasad2587@gmail.com>
2024-10-25 15:29:39 +0200

    add the trim to the login schema

--- src/schemas/loginFormSchema.ts
+++ src/schemas/loginFormSchema.ts
@@ -4,9 +4,11 @@ export const LoginFormSchema = z.object({
   email: z
     .string()
     .min(11, "your email are invalid or too small")
-    .max(255, "your email are invalid or too long"),
+    .max(255, "your email are invalid or too long")
+    .trim(),
   password: z
     .string()
     .min(8, "your password are invalid or too small")
-    .max(20, "your password are invalid or too long"),
+    .max(20, "your password are invalid or too long")
+    .trim(),
 });
