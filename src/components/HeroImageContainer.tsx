123a8b27293416f30817f4576db4ebef4e3edde9
Zakaria Asad <zasad2587@gmail.com>
2024-11-01 16:14:29 +0200

    Refactor: make the HeroImage Container responsive and add to it the search bar

--- src/components/HeroImageContainer.tsx
+++ src/components/HeroImageContainer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import heroImage from "../../public/Rectangle 31.png";
+import Search from "./Search";
 
 const HeroImageContainer = () => {
   return (
@@ -11,10 +12,11 @@ const HeroImageContainer = () => {
         className="object-cover w-full h-full"
       />
       <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50 p-4">
-        <h1 className="text-[24px] leading-[32px] sm:text-[32px] sm:leading-[40px] md:text-[40px] md:leading-[48px] lg:text-[48px] lg:leading-[57.6px] font-bold max-w-4xl">
-          Transform your vision into reality with our expert construction and
-          landscaping services where quality meets creativity.
+        <h1 className="whitespace-nowrap text-[24px] leading-[32px] sm:text-[32px] sm:leading-[40px] md:text-[40px] md:leading-[48px] lg:text-[48px] lg:leading-[57.6px] font-bold">
+          Transform your vision into reality with <br /> our expert construction and
+          landscaping <br /> services where quality meets creativity.
         </h1>
+        <Search isItHeroImage={true}/>
       </div>
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,18 @@
-const Search = () => {
+import classNames from "classnames";
+
+interface Props {
+  isItHeroImage?: boolean;
+}
+
+const Search = ({ isItHeroImage = false }: Props) => {
   return (
     <input
-      className="hidden xl:block rounded-lg w-full md:w-[358px] h-[2.5rem] md:h-[46px] pl-4 md:pl-10 bg-white border border-[#EEF9F3] 
-             focus:ring-2 focus:ring-[#EEF9F3] focus:outline-none
-             shadow-[0px_10px_30px_rgba(0,0,0,0.15)] transform transition-all duration-300
-             hover:translate-y-[-2px] hover:shadow-[0px_15px_40px_rgba(0,0,0,0.25)]"
+      className={classNames({
+        "rounded-lg w-full md:w-[358px] h-[2.5rem] md:h-[46px] pl-4 md:pl-10 bg-white border border-[#EEF9F3] focus:ring-2 focus:ring-[#EEF9F3] focus:outline-none shadow-[0px_10px_30px_rgba(0,0,0,0.15)] transform transition-all duration-300 hover:translate-y-[-2px] hover:shadow-[0px_15px_40px_rgba(0,0,0,0.25)]":
+          true,
+        "block mt-6 max-w-[358px]": isItHeroImage === true,
+        "hidden xl:block": isItHeroImage === false,
+      })}
       type="text"
       placeholder="Search..."
     />
