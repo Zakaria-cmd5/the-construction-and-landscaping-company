219858f384dcb44a5ca21e1a850c12e76376c31f
Zakaria Asad <zasad2587@gmail.com>
2024-11-01 16:25:55 +0200

    Refactor: make the FeaturesHeading responsive

--- src/components/FeaturesHeading.tsx
+++ src/components/FeaturesHeading.tsx
@@ -10,22 +10,24 @@ interface Props {
 const FeaturesHeading = ({ children, featureColor }: Props) => {
   const { darkMode } = useDarkMode();
 
-  const sectionClassName = classNames({
-    "bg-opacity-50 h-[188px] w-[1920px] flex items-center sm:w-full sm:h-[150px]":
-      true,
-    "bg-[#59ED9F]": featureColor === "feature-image",
-    "bg-[#0E7E83]": featureColor === "feature-explain",
-  });
-
-  const spanClassName = classNames({
-    "lg:text-[64px] p-4 lg:leading-[76.8px] sm:text-[48px] sm:leading-[60px] text-[32px] leading-[40px]":
-      true,
-    "text-white": darkMode,
-  });
-
   return (
-    <section className={sectionClassName}>
-      <span className={spanClassName}>{children}</span>
+    <section
+      className={classNames({
+        "bg-opacity-50 flex items-center lg:justify-normal justify-center w-full h-[120px] sm:h-[150px] lg:h-[188px] px-4":
+          true,
+        "bg-[#59ED9F]": featureColor === "feature-image",
+        "bg-[#0E7E83]": featureColor === "feature-explain",
+      })}
+    >
+      <span
+        className={classNames({
+          "text-[24px] leading-[32px] sm:text-[36px] sm:leading-[48px] lg:text-[48px] lg:leading-[60px] xl:text-[64px] xl:leading-[76.8px]":
+            true,
+          "text-white": darkMode,
+        })}
+      >
+        {children}
+      </span>
     </section>
   );
 };
