f79981f9f8bb4223b3e63edcda35a183322d194a
Zakaria Asad <zasad2587@gmail.com>
2024-11-01 16:29:16 +0200

    Refactor: make SectionsHeading responsive

--- src/components/SectionsHeading.tsx
+++ src/components/SectionsHeading.tsx
@@ -14,10 +14,10 @@ const SectionsHeading = ({ children, featureId }: Props) => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center">
       <div className="flex mt-5 items-center">
-        <div className="bg-[#14B05D] w-[40px] h-[30px]" />
+        <div className="bg-[#14B05D] w-[30px] h-[20px] md:w-[40px] md:h-[30px]" />
         <span
           className={classNames({
-            "ml-2 lg:text-[48px] lg:leading-[57.6px] sm:text-[36px] sm:leading-[43.2px] text-[28px] leading-[34px]":
+            "ml-2 text-[24px] leading-[32px] sm:text-[28px] sm:leading-[34px] lg:text-[36px] lg:leading-[43.2px] xl:text-[48px] xl:leading-[57.6px]":
               true,
             "text-white": darkMode,
           })}
@@ -27,8 +27,8 @@ const SectionsHeading = ({ children, featureId }: Props) => {
       </div>
       <Link
         href={`/features/${featureId}`}
-        className="w-[260px] h-[73px] flex items-center justify-center bg-[#074143] mr-5 mt-5 rounded-lg text-[#EEF9F3] text-[28px] leading-[33.89px] font-medium
-          sm:w-[200px] sm:h-[60px] sm:text-[24px] sm:leading-[29px]"
+        className="w-[200px] h-[60px] mt-5 flex items-center justify-center bg-[#074143] rounded-lg text-[#EEF9F3] text-[20px] leading-[25px] font-medium
+          sm:w-[220px] sm:h-[65px] sm:text-[22px] sm:leading-[28px] md:w-[240px] md:h-[70px] md:text-[24px] md:leading-[30px] lg:w-[260px] lg:h-[73px] lg:text-[28px] lg:leading-[33.89px]"
       >
         VIEW ALL
       </Link>
