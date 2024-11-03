1c69f02b117440ae17008fbe6ac058c9533c73f5
Zakaria Asad <zasad2587@gmail.com>
2024-11-03 13:59:16 +0200

    Simplfy the notification button

--- src/components/NotificationButton.tsx
+++ src/components/NotificationButton.tsx
@@ -19,13 +19,8 @@ const NotificationButton = ({
 }: Props) => {
   const { token } = useFcmToken();
 
-  const handleTestNotification = async () => {
-    try {
-      const data = await sendNotification(token, heading, title, "/");
-      console.log("Notification sent:", data);
-    } catch (error) {
-      console.error("Failed to send test notification:", error);
-    }
+  const handleNotificationSend = async () => {
+    await sendNotification(token, heading, title, "/");
   };
 
   return (
@@ -33,7 +28,7 @@ const NotificationButton = ({
       type={buttonType}
       disabled={pending}
       className="bg-[#2BE784] hover:bg-[#121C17] text-[#121C17] hover:text-[#2BE784] font-medium rounded-lg w-full max-w-[204px] h-[49px] mx-auto flex justify-center items-center hover:scale-110"
-      onClick={handleTestNotification}
+      onClick={handleNotificationSend}
     >
       {pending ? <Spinner /> : buttonLabel}
     </button>
